540b39137eff0ca4bb6fe8e3ffb5c63d06cb2dab
jandaes <158464459@qq.com>
2017-08-02 18:00:17 +0800

    SQL更新

--- src/main/resources/liujilu.sql
+++ src/main/resources/liujilu.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : localhost
-Source Server Version : 50622
-Source Host           : 127.0.0.1:3306
+Source Server Version : 50627
+Source Host           : localhost:3306
 Source Database       : liujilu
 
 Target Server Type    : MYSQL
-Target Server Version : 50622
+Target Server Version : 50627
 File Encoding         : 65001
 
-Date: 2017-07-31 03:04:58
+Date: 2017-08-02 17:59:45
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,7 +20,7 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
 CREATE TABLE `sys_user` (
-  `id` varchar(10) NOT NULL COMMENT '编号',
+  `id` varchar(20) NOT NULL COMMENT '编号',
   `name` varchar(50) DEFAULT NULL COMMENT '姓名',
   `nickname` varchar(15) DEFAULT NULL COMMENT '昵称',
   `email` varchar(15) DEFAULT NULL COMMENT '邮箱',
@@ -35,5 +35,6 @@ CREATE TABLE `sys_user` (
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES ('1', '名字', '昵称', '158@qq.com', '158464', '密文密码', '2017-07-30 02:10:25', '2017-07-31 02:10:32', '1');
+INSERT INTO `sys_user` VALUES ('1', '名字', '昵称', '158@qq.com', '158464', '密文密码', '2017-07-30 02:10:25', '2017-07-31 02:10:32', '0');
 INSERT INTO `sys_user` VALUES ('2', '测试', '测试昵称', '154@qq.com', '456', '测试密码', '2017-07-31 02:26:51', '2017-07-31 02:26:49', '1');
+INSERT INTO `sys_user` VALUES ('892661942181748736', '张三', 'zhangsan', 'zs@q.com', '123456', '123456', '2017-08-02 16:22:47', null, '0');
